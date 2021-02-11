8e2353c531ad7caeff331393b362d28251dfb102
Nabila <nabila.abraham@gmail.com>
2021-02-11 18:18:38 -0500

    editing folder structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.1.0'
-PACKAGE_NAME = 'pandas-erd'
+PACKAGE_NAME = 'pandaserd'
 AUTHOR = 'Nabila Abraham'
 AUTHOR_EMAIL = 'nabila.abraham@gmail.com'
 URL = 'https://github.com/nabsabraham/pandas-erd'
