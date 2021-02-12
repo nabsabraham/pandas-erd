2fabe9def33e5dc5103b218eeb135bcadc396412
Nabila <nabila.abraham@gmail.com>
2021-02-11 22:08:59 -0500

    patch fix to allow imports

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 PACKAGE_NAME = 'pandaserd'
 AUTHOR = 'Nabila Abraham'
 AUTHOR_EMAIL = 'nabila.abraham@gmail.com'
