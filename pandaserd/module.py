b8d148a2916273d17cf11e1a90fd2608a649f44a
Nabila <nabila.abraham@gmail.com>
2021-02-14 14:15:22 -0500

    relationship tracker, arrows, check if edge column is in the table

--- pandaserd/module.py
+++ pandaserd/module.py
@@ -9,12 +9,13 @@ class Table:
     filename: (str) - filename to save dot code - default is output.txt
     '''
 
-    def __init__(self, table, table_name):
+    def __init__(self, table, table_name, **kwargs):
         if isinstance(table, pd.core.frame.DataFrame):
             # pass a whole dataframe
             meta = table.dtypes
             meta = meta.to_frame().reset_index()
             meta.columns = ['column_name', 'data_type']
+            self.table_columns = set(meta.column_name)
             self.meta_info = meta.values.tolist()
 
         else:
@@ -26,7 +27,7 @@ class Table:
         self.pad = 5
         self.align = 'left'
         self.font_color = 'grey60'
-        self.bg_color = 'grey' # ['lightblue', 'skyblue', 'pink', 'lightyellow', 'grey']
+        self.bg_color = kwargs.get('bg_color', 'grey') # ['lightblue', 'skyblue', 'pink', 'lightyellow', 'grey']
         self.__construct__()
 
     def __construct__(self):
@@ -63,83 +64,113 @@ class ERD:
     """
     def __init__(self):
         self.table_tracker = {}
-        self.table_gen_code = ['''
-    digraph G {
-            graph [
-                nodesep=0.5;
-                rankdir="LR";
-                cencentrate=true;
-                splines="spline";
-                fontname="Helvetica";
-                pad="0.2,0.2",
-                label="",
+        self.rel_tracker = set()
+        self.table_gen_code = ['''digraph G {
+        graph [
+            nodesep=0.5;
+            rankdir="LR";
+            cencentrate=true;
+            splines="spline";
+            fontname="Helvetica";
+            pad="0.2,0.2",
+            label="",
 
-            ];
+        ];
 
-    node [shape=plain, fontname="Helvetica"];
-    edge [
-        dir=both,
-        fontsize=12,
-        arrowsize=0.9,
-        penwidth=1.0,
-        labelangle=32,
-        labeldistance=1.8,
-        fontname="Helvetica"
-    ];''']
+        node [shape=plain, fontname="Helvetica"];
+        edge [
+            dir=both,
+            fontsize=12,
+            arrowsize=0.9,
+            penwidth=1.0,
+            labelangle=32,
+            labeldistance=1.8,
+            fontname="Helvetica"
+        ];''']
 
     def add_table(self, df, table_name):
         table = Table(table=df, table_name=table_name)
         self.table_tracker[table_name] = table
         self.table_gen_code.append(table.res)
+        return table
 
     def print(self):
         self.table_gen_code.append('}')
         print(self.table_gen_code)
 
-    def create_edge(self, table1_name, table2_name, **kwargs):
-        '''
-
-        :param table1_name: Table object
-        :param table2_name: Table object
-        :param left_cardinality: '*' or '+'; default is none
-        :param: right_cardinality: '*' or '+'; default is none
-
-
-        '''
-        try:
-            table1 = self.table_tracker[table1_name]
-        except:
-            print(f' table {table1_name} not found, create one with add_table method')
-            return
-
-        try:
-            table2 = self.table_tracker[table2_name]
-        except:
-            print(f' table {table2_name} not found, create one with add_table method')
-            return
-
-        left_on = kwargs.get('left_on', '')
-        right_on = kwargs.get('right_on', '')
-        left_cardinality = kwargs.get('left_cardinality', None)
-        right_cardinality = kwargs.get('right_cardinality', None)
-
-        if left_cardinality == '*':
+    def __make_edge__(self):
+        if self.left_cardinality == '*':
             arrowtail = 'ocrow'
-        elif left_cardinality == '+':
+        elif self.left_cardinality == '+':
             arrowtail = 'noneotee'
         else:
             arrowtail = 'none'
 
-        if right_cardinality == '*':
+        if self.right_cardinality == '*':
             arrowhead = 'ocrow'
-        elif right_cardinality == '+':
+        elif self.right_cardinality == '+':
             arrowhead = 'noneotee'
         else:
             arrowhead = 'none'
 
-        rel = f'''\n\t {table1.table_name}:{left_on}->{table2.table_name}:{right_on} [ 
-                arrowhead={arrowhead}, arrowtail={arrowtail}];'''
-        self.table_gen_code.append(rel)
+        # if you didn't set cardinalities, you can make arrows
+        if self.left_cardinality is None and self.right_cardinality is None:
+            if self.left_arrow and self.right_arrow is False:
+                rel = f'''\n\t {self.left.table_name}:{self.left_on}->{self.right.table_name}:{self.right_on} [
+                        arrowhead="none"];'''
+            elif self.right_arrow and self.left_arrow is False:
+                rel = f'''\n\t {self.left.table_name}:{self.left_on}->{self.right.table_name}:{self.right_on} [
+                            arrowtail="none"];'''
+            elif self.left_arrow and self.right_arrow:
+                rel = f'''\n\t {self.left.table_name}:{self.left_on}->{self.right.table_name}:{self.right_on};'''
+            else:
+                rel = f'''\n\t {self.left.table_name}:{self.left_on}->{self.right.table_name}:{self.right_on};'''
+
+        else:  # use the cardinalities
+            rel = f'''\n\t {self.left.table_name}:{self.left_on}->{self.right.table_name}:{self.right_on} [ 
+                        arrowhead={arrowhead}, arrowtail={arrowtail}];'''
+
+        return rel
+
+    def create_rel(self, left_table_name, right_table_name, left_on=None, right_on=None, on=None, **kwargs):
+        # get the tables referenced by their name
+        try:
+            self.left = self.table_tracker[left_table_name]
+        except:
+            print(f' table {left_table_name} not found, create one with add_table method')
+            return
+
+        try:
+            self.right = self.table_tracker[right_table_name]
+        except:
+            print(f' table {right_table_name} not found, create one with add_table method')
+            return
+
+        # use the on or left_on/right_on like pandas merge
+        if on is not None:
+            self.left_on = on
+            self.right_on = on
+        else:
+            self.left_on = left_on
+            self.right_on = right_on
+
+        # check that the on columns exist in the tables:
+        assert self.left_on in self.left.table_columns, f'Column {self.left_on} not in left table: {self.left.table_name}'
+        assert self.right_on in self.right.table_columns, f'Column {self.right_on} not in right table: {self.right.table_name}'
+
+        self.left_cardinality = kwargs.get('left_cardinality', None)
+        self.right_cardinality = kwargs.get('right_cardinality', None)
+        self.left_arrow = kwargs.get('left_arrow', None)
+        self.right_arrow = kwargs.get('right_arrow', None)
+
+        rel_name = f'{left_table_name}:{self.left_on}->{right_table_name}:{self.right_on}'
+        # check if the rel already exist
+        if rel_name in self.rel_tracker:
+            print(f' Edge {rel_name} already exists, skipping this edge creation.')
+        else:
+            self.rel_tracker.add(rel_name)
+            rel = self.__make_edge__()
+            self.table_gen_code.append(rel)
 
     def write_to_file(self, filename='output.txt'):
         '''
