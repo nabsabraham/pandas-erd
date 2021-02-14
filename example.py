7e6d7f675387158c42e4ae64ca1dff1f5871cd47
Nabila <nabila.abraham@gmail.com>
2021-02-14 14:15:55 -0500

    example showing arrow/flowchart style diagram

--- example.py
+++ example.py
@@ -7,7 +7,9 @@ df2 = pd.DataFrame(data=[[6342, 124123124124, '1992-01-02', 'K012V4'], [3124, 15
 df2.columns = ['PERSON', 'CREDIT_CARD', 'DOB', 'POSTAL_CODE']
 
 erd = ERD()
-erd.add_table(df1, 'PERSON')
-erd.add_table(df2, 'CREDIT_CARD')
-erd.create_edge('PERSON', 'CREDIT_CARD', left_on='PERSON', right_on='PERSON', right_cardinality='*')
+t1 = erd.add_table(df1, 'PERSON')
+t2 = erd.add_table(df2, 'CREDIT_CARD')
+erd.create_rel('PERSON', 'CREDIT_CARD', on='PERSON', left_cardinality='+', right_cardinality='*')
+erd.create_rel('PERSON', 'CREDIT_CARD', left_on='AGE', right_on='DOB', right_arrow=True, left_arrow=False)
+
 erd.write_to_file('output.txt')
